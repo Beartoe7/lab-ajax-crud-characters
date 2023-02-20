47c2d502d41a216f8a4db72c2b2beb12a032becd
Beartoe7 <64451243+Beartoe7@users.noreply.github.com>
2023-02-20 09:11:23 +0100

    -

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,9 +1,28 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler('http://localhost:8001');
 
 window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+  
+  document.getElementById('fetch-all').addEventListener('click', function () {
+    charactersAPI.getFullList().then((allCharacters)=> {
+      const parentDiv = document.querySelector(".characters-container");
+      const hiddenDivOnFetch = document.getElementById(
+        "character-info-to-hide"
+      );
+    allCharacters.forEach((character)=> {
+      let oneCharacter = document.createElement("div");
+      oneCharacter.className = "character-info";
 
+      //Mostrar nombre
+      let newDivName = document.createElement("div");
+      newDivName.classList.add("name");
+      newDivName.innerHTML = `Name: ${character.name}`;
+      oneCharacter.appendChild(newDivName);
+
+      parentDiv.appendChild(oneCharacter);
+    });
+    hiddenDivOnFetch.style.display = "none";
   });
+});
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
 
