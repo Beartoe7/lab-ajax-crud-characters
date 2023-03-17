df3a99694007e7e05b06eda4b30e9daedf2196a1
Beartoe7 <64451243+Beartoe7@users.noreply.github.com>
2023-03-17 16:49:44 +0100

    ok

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,28 +1,59 @@
 
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
-  }
+	constructor(baseUrl) {
+		this.api = axios.create({ baseURL: baseUrl });
+		this.BASE_URL = baseUrl;
+	}
 
-  async getFullList () {
-    const res = await axios.get(this.BASE_URL + "/characters")
-    return res.data
-  }
+	getFullList() {
+		return this.api
+			.get('/characters')
+			.then((response) => {
+				console.log('Retrieving all characters');
+				return response.data;
+			})
+			.catch(() => {
+				console.log('Something went wrong, there are no characters');
+				return 'Something went wrong, there are no characters';
+			});
+	}
 
-  async getOneRegister (id) {
-    const res = await axios.get(this.BASE_URL + "/characters/" + id)
-    return res.data
-  }
+	getOneRegister(id) {
+		return this.api
+			.get(`/characters/${id}`)
+			.then((response) => {
+				console.log('Character FOUND by ID');
+				return response.data;
+			})
+			.catch(() => {
+				console.log('Character NOT found by ID');
+				return 'Character not found by ID';
+			});
+	}
 
-  createOneRegister () {
+	createOneRegister(newCharacter) {
+		return this.api.post('/characters', newCharacter).then(() => {
+			console.log('New character created');
+		});
+	}
 
-  }
+	updateOneRegister(id, editedCharacter) {
+		return this.api
+			.put(`${this.BASE_URL}/characters/${id}`, editedCharacter)
+			.then(() => {
+				console.log('Character has been successfully updated');
+				return 'Character has been successfully updated';
+			})
+			.catch(() => 'Character not found');
+	}
 
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
-  }
+	deleteOneRegister(id) {
+		return this.api
+			.delete(`/characters/${id}`)
+			.then(() => {
+				console.log('Character has been successfully deleted');
+				return 'Character has been successfully deleted';
+			})
+			.catch(() => 'Character not found');
+	}
 }
